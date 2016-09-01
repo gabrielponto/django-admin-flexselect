ed0928406bd2f00064b9a2029663402cc9f54ec7
Gabriel Oliveira <dev@gabrieloliveira.net>
2016-09-01 16:03:21 -0300

    muda a versao e atribuicao de autor

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='django-admin-flexselect',
-    version='0.4.0',
-    author=u'Rune Kaagaard',
+    version='0.5.0',
+    author=u'Rune Kaagaard - Gabriel Oliveira',
     author_email='rumi.kg@gmail.com',
     packages=['flexselect'],
-    url='https://github.com/runekaagaard/django-admin-flexselect',
+    url='https://github.com/gabrielponto/django-admin-flexselect',
     license='Public Domain',
     description='Dynamic select fields for the Django Admin that just works.',
     long_description=open('README.markdown').read(),
